f2b13e9b9b7e2c6f650773dba489b53af8e22746
Ian Salmon <31865791+IanGSalmon@users.noreply.github.com>
2019-05-15 15:00:19 -0600

    Update getHotelById.js

--- sam-app/src/lambda/getHotelById.js
+++ sam-app/src/lambda/getHotelById.js
@@ -11,66 +11,39 @@ const pool = new Pool({
   max: 50,
 });
 
-exports.handler = (event, context, callback) => {
-  console.log(event);
-  const { id } = event.pathParameters;
-  const queryString = `SELECT * FROM hotel WHERE id=${id}`;
-
-  pool.query(queryString, (err, result) => {
-    if (err) {
-      console.log(err);
-    } else {
-      callback(null, {
-        statusCode: 200,
-        body: result
-      });
+module.exports.handler = (event, context, callback) => {
+  context.callbackWaitsForEmptyEventLoop = false;
+    
+//   For deployment uncomment lns 18-19
+//   const { id } = event.pathParameters;
+//   const queryString = `SELECT * FROM hotel WHERE id=${id}`;
+  
+  let client;
+  pool.connect().then(c => {
+    client = c;
+//      for deployement uncomment line 25, delete line 26
+//       return client.query(`${queryString}`);
+    return client.query("SELECT * FROM hotel WHERE id=1");
+  }).then(res => {
+    client.release();
+    const response =  {
+      "isBase64Encoded": false,
+      "statusCode": 200,
+      "body": JSON.stringify(res.rows),
+      "headers": {
+                  "Access-Control-Allow-Origin": "*",
+                  "Access-Control-Allow-Credentials": true
+                  }
+      }
+    callback(null, response);
+  }).catch(error => {
+  console.log("ERROR", error);
+    const response =  {
+      "isBase64Encoded": false,
+      "statusCode": 500,
+      "body": JSON.stringify(error)
     }
+
+    callback(null, response);
   });
 };
-
-/*
-const { Pool } = require('pg')
-
-const pool = new Pool({
-    host: 'rickadvisor-about-db-replica.c52xkeof6emi.us-east-2.rds.amazonaws.com',
-    database: 'hotels',
-    user: 'ian',
-    password: 'rhodyhackerdog',
-    port: 5432,
-    max: 1,
-    min: 0,
-    idleTimeoutMillis: 300000,
-    connectionTimeoutMillis: 1000
-});
-
-module.exports.handler = (event, context, callback) => {
-    context.callbackWaitsForEmptyEventLoop = false;
-
-    let client;
-    pool.connect().then(c => {
-        client = c;
-        return client.query("SELECT * FROM hotel WHERE id=1");
-    }).then(res => {
-        client.release();
-        const response =  {
-            "isBase64Encoded": false,
-            "statusCode": 200,
-            "body": JSON.stringify(res.rows),
-            "headers": {
-                        "Access-Control-Allow-Origin": "*",
-                        "Access-Control-Allow-Credentials": true
-                        }
-        }
-        callback(null, response);
-    }).catch(error => {
-        console.log("ERROR", error);
-        const response =  {
-            "isBase64Encoded": false,
-            "statusCode": 500,
-            "body": JSON.stringify(error)
-        }
-
-        callback(null, response);
-    });
-};
-*.
